7ef0a5d112e358373b55386e121c40e5099f965e
mohan <gmrc4u@gmail.com>
2019-06-27 12:10:47 +0530

    branching commit

--- add.java
+++ add.java
@@ -2,7 +2,7 @@ public class AddTwoNumbers {
 
    public static void main(String[] args) {
         
-      int num1 = 10, num2 = 15, num3 = 20, sum;
+      int num1 = 12, num2 = 20, num3 = 25, sum;
       sum = num1 + num2 + num3;
 
       System.out.println("Sum of these numbers: "+sum);
